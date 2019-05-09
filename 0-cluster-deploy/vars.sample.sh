9a804a960bd181cc8a01efc3449dd6f5cb98cbd9
Ben Coleman <benc.uk@gmail.com>
2019-05-09 11:24:51 +0100

    Updated deployment scripts

--- 0-cluster-deploy/vars.sample.sh
+++ 0-cluster-deploy/vars.sample.sh
@@ -1,5 +1,5 @@
 #
-# RENAME ME! to vars.sh
+# COPY AND RENAME ME! to vars.sh
 #
 
 # Change these
@@ -7,9 +7,16 @@ resGrp=""
 location=""
 clusterName=""
 logWorkspaceName="" # MUST BE UNIQUE!
+kubeVersion="1.13.5"
 
 # Maybe change these
 vmSize="Standard_D2s_v3"
-kubeVersion="1.12.4"
+minNodes="1"
+maxNodes="5"
+
 vnetName="aks-vnet"
 subnetName="aks-subnet"
+vnodesSubnetName="vnodes-subnet"
+
+winAdminUser="winadmin"
+winAdminPwd="HUw!yKfDd-B^6j@D"
