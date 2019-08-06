c1e2a0607ebf198fc0cd5239fa3c1cd5147c0a66
Ben Coleman <benc.uk@gmail.com>
2019-08-06 15:14:36 +0100

    added sentiment

--- common/remove.sh
+++ common/remove.sh
@@ -4,8 +4,9 @@ kubectl delete statefulset/mongodb
 kubectl delete deploy/mongodb
 kubectl delete deploy/data-api
 kubectl delete deploy/frontend
+kubectl delete deploy/sentiment
 kubectl delete svc/frontend-svc
 kubectl delete svc/data-api-svc
 kubectl delete svc/mongodb-svc
+kubectl delete svc/sentiment-svc
 kubectl delete ingress/smilr-ingress
-kubectl get all
