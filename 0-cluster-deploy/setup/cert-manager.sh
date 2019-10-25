225e62c09cca310c87ca21c88026d732707780a2
Ben Coleman <benc.uk@gmail.com>
2019-10-25 09:45:24 +0100

    Cert manager updates

--- 0-cluster-deploy/setup/cert-manager.sh
+++ 0-cluster-deploy/setup/cert-manager.sh
@@ -12,11 +12,24 @@ kubectl create namespace cert-manager
 #
 kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v$CERT_MGR_VER/cert-manager.yaml --validate=false
 
-echo "Waiting 60 secs, before creating issuers..."
-sleep 60
+echo "Waiting 90 secs, before creating issuers..."
+sleep 90
 
 #
-# Deploy issuers
+# Deploy HTTP issuers
+#
+kubectl apply -f issuer-prod.yaml
+kubectl apply -f issuer-staging.yaml
+
+#
+# For DNS issuers ONLY
+# REMOVE if you're not using wildcard certs with a wildcard DNS zone
+#
+$SECRET="{{change-me}}"
+kubectl create secret generic azuredns-config --from-literal=CLIENT_SECRET=$SECRET -n cert-manager
+
+#
+# Apply issuers
 #
 kubectl apply -f issuer-prod.yaml
 kubectl apply -f issuer-staging.yaml
